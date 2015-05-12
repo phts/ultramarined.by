e9720f555841e1432208ed9c8a1473f4d9decda7
Phil Tsarik <phil.tsarik@gmail.com>
2015-05-13 01:16:56 +0300

    Add custom styles for mobile browsers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
+var isMobile = !!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+if (isMobile) {
+  $('head').append("<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>");
+}
+
 $(function() {
+  if (isMobile) {
+    $("html").addClass("mobile");
+  }
+
   var $startPage = $("#start-page");
   var $overlay = $("#overlay");
   var $header = $("#header");
@@ -15,4 +25,3 @@ $(function() {
     }
   });
 });
-
