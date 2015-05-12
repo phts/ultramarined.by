7dfe5ce5775b8b58a8ab65febcf36eeca7e9205f
Phil Tsarik <phil.tsarik@gmail.com>
2015-05-13 00:53:22 +0300

    Cache some variables

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 $(function() {
-  var startPage = $("#start-page");
+  var $startPage = $("#start-page");
+  var $overlay = $("#overlay");
+  var $header = $("#header");
 
   $(window).on('scroll', function() {
     var scroll = $(window).scrollTop();
-    var startChange = 2*startPage.height()/3
+    var startChange = 2 * $startPage.height() / 3;
     if (scroll > startChange) {
-      $('#overlay').addClass("hidden");
-      $('#header').show();
+      $overlay.addClass("hidden");
+      $header.show();
     } else {
-      $('#overlay').removeClass("hidden");
-      $('#header').hide();
+      $overlay.removeClass("hidden");
+      $header.hide();
     }
   });
 });
