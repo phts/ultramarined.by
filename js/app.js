9151eafc5396a6a606ff4b708d987a6e0784c303
Phil Tsarik <phil.tsarik@gmail.com>
2015-05-14 17:56:00 +0300

    Refactor CSS classes
    * Rename "page" to "slide"
    * Set classes to <header> and <footer>

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ $(function() {
     $body.addClass("mobile");
   }
 
-  var $startPage = $("#start-page");
+  var $startSlide = $("#start-slide");
   var $overlay = $("#overlay");
 
   if (!isMobile) {
     var watchOverlay = function() {
       var scroll = $(window).scrollTop();
-      var removeOverlayAt = 2 * $startPage.height() / 3;
+      var removeOverlayAt = 2 * $startSlide.height() / 3;
       if (scroll > removeOverlayAt) {
         $body.removeClass("with-overlay");
       } else {
@@ -29,14 +29,14 @@ $(function() {
     $body.removeClass("with-overlay");
   }
 
-  $startPage.on('click', function() {
+  $startSlide.on('click', function() {
     var red = Math.floor((Math.random() * 50) + 67);
     var green = Math.floor((Math.random() * 50) + 27);
     var blue = Math.floor((Math.random() * 50) + 186);
     if (!isMobile) {
       $overlay.css("background-color", "rgb("+red+", "+green+", "+blue+")");
     } else {
-      $startPage.css("background-color", "rgb("+red+", "+green+", "+blue+")");
+      $startSlide.css("background-color", "rgb("+red+", "+green+", "+blue+")");
     }
   });
 
