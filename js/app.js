cdda57d2de165dd0b119574b71b3cadca011625d
Phil Tsarik <phil.tsarik@gmail.com>
2015-05-24 00:47:38 +0300

    Transform to "Coming soon" page

--- js/app.js
+++ js/app.js
@@ -3,26 +3,3 @@ var isMobile = !!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/
 if (isMobile) {
   $('head').append("<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>");
 }
-
-$(function() {
-  var $body = $("body");
-  if (isMobile) {
-    $body.addClass("mobile");
-  }
-
-  var $startSlide = $("#start-slide");
-  var $contactsSlide = $("#contacts-slide");
-
-  var watchScroll = function() {
-    var scroll = $(window).scrollTop();
-    var startSlideHideAt = $startSlide.height();
-    if (scroll > $contactsSlide.position().top-($contactsSlide.height()/4)) {
-      $body.addClass("header-shown footer-shown");
-    } else {
-      $body.removeClass("header-shown footer-shown");
-    }
-  }
-  watchScroll();
-  $(window).on('scroll', watchScroll);
-
-});
