0b02b561b6fd988a21dcbb059bcbdf839a3c2e12
Phil Tsarik <phil.tsarik@gmail.com>
2015-05-25 12:50:17 +0300

    Remove mobile detection in JS and move meta tag to HTML
    Because it doesn't affect on desktop browsers.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
-var isMobile = !!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-if (isMobile) {
-  $('head').append("<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>");
-}
-
 $(function() {
   var $body = $("body");
-  if (isMobile) {
-    $body.addClass("mobile");
-  }
-
   var $startSlide = $("#start-slide");
   var $contactsSlide = $("#contacts-slide");
 
